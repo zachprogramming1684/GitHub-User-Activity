6eae5b4d71a07b00a6b6286fd7981eaee172ca67
smgzach <zm989392@gmail.com>
2025-05-08 17:31:17 -0400

    got data from the api, need to parse through it

--- GitHub User Activity/src/Main.java
+++ GitHub User Activity/src/Main.java
@@ -1,9 +1,33 @@
+import java.io.IOException;
+import java.net.*;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
+import com.google.gson.Gson;
 
-public class Main {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+public class Main 
+{
+	public static void main(String[] args) throws Exception
+	{
+		/*
+		 * Build a CLI to fetch recent activity of a GitHub user and display it in the terminal
+		 * Should accept GitHub username as an argument, fetch the user's recent activity using the
+		 * GitHub API and display it in the terminal.
+		 */
 
+		HttpRequest getRequest = HttpRequest.newBuilder()
+				.uri(new URI("https://api.github.com/users/zachprogramming1684/events"))
+				.header("accept", "application/vnd.github+json")
+				.build();
+				
+		HttpClient h = HttpClient.newHttpClient();
+		HttpResponse<String> getResponse = h.send(getRequest, BodyHandlers.ofString());
+		System.out.println(getResponse.body());
+		
+		Gson g = new Gson();
+		
 	}
 
 }
